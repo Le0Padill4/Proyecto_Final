499e02336e8e17880b6b40dbacbcb2b91b76264c
Leonardo <leoandrespadilla@gmail.com>
2026-07-07 18:58:27 -0500

    Cree los archivos en donde trabajar y la clase personaje
    Cree los archivos en donde trabajar y la clase personaje el archivo hpp donde se define a personaje y sus atributos

---
+++ personaje.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+class Personaje {
+protected:
+    std::string nombre;
+    int hp;
+    int hpmax;
+    int ki;
+    int kimax;
+    int ataque;
+    int defensa;
+    bool defiende;
+    std::string nataque;
+    std::string nataque_especial;
+    int ki_especial;
+
+public:
+    Personaje(const std::string& nombre, int hpmax, int kimax, int ataque, int defensa);
+    virtual ~Personaje();
+
+    std::string getNombre() const;
+    int getHp() const;
+    int getHpMax() const;
+    int getKi() const;
+    int getKiMax() const;
+    int getAtaque() const;
+    int getDefensa() const;
+    int getKiEspecial() const;
+    bool getDefiende() const;
+    std::string getNataque() const;
+    std::string getNataqueEspecial() const;
+
+    void setNombre(const std::string& nombre);
+    void setHp(int hp);
+    void setataque(int ataque);
+    void setdefensa(int defensa);
+    void setKi(int ki);
+    bool vive() const;
+    void mostrar() const;
+};
+
+void Personaje::mostrar() const {
+    std::cout << "Nombre: " << getNombre() << std::endl;
+    std::cout << "Vida: " << getHp() << "/" << getHpMax() << std::endl;
+    std::cout << "Ki: " << getKi() << "/" << getKiMax() << std::endl;
+    std::cout << "Ataque: " << getAtaque() << std::endl;
+    std::cout << "Defensa: " << getDefensa() << std::endl;
+
+    if (getDefiende()) {
+        std::cout << "Estado: Defendiendo" << std::endl;
+    } else {
+        std::cout << "Estado: Normal" << std::endl;
+    }
+}
